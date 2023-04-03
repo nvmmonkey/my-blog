a6824fbaaacc82b87648fc2422c8aaca6c0fbd1d
Kit <nvmmonkey@github.com>
2023-04-03 12:49:49 +0800

    ⚠️Edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "blog.kitkong",
+  title: "Kit Kong",
   tagline: "Welcoome to my blog",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
